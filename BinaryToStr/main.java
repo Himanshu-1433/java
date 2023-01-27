49a977d9adc06e8269631d446090540c8003936a
= <=>
2023-01-27 12:59:29 +0530

    commit

--- BinaryToStr/main.java
+++ BinaryToStr/main.java
@@ -14,7 +14,7 @@ public class main implements secure{
     @Override
     public void convert() {
         String input =  input();
-        System.out.println(input.length());
+        // System.out.println(input.length());
         char arr[] = new char[input.length()];
         int binaryArr[] = new int[input.length()];
         String txt;
@@ -23,22 +23,43 @@ public class main implements secure{
             arr[i] = input.charAt(i);
             txt = Integer.toBinaryString(arr[i]);
             binaryArr[i] = Integer.parseInt(txt);
+            // System.out.println(binaryArr[i]);
         }
-        for(int i = 0;i<input.length();i++)
-        {
-            System.out.print(binaryArr[i]);
-        }
-        int i = 0;
-        int t = 0;
-        char[] alphabet = {'a','b','c','d','e'};
-        while(i<binaryArr.length){
-            for()
-            {
 
-            }
-            i++;
-        }
+        // for(int i = 0;i<input.length();i++)
+        // {
+        //     System.out.print(binaryArr[i]);
+        // }
         
+        int one = 0;
+        int zero = 0;
+        char[] alphabet = {'a','b','c','d','e'};
+        for(int i = 0; i<input.length();i++){
+         int num = binaryArr[i];
+         for(int j = 0;j<=5;j++)
+         {
+                
+                
+                System.out.println(num);
+                // if(num == 100000)
+                // {
+                //         // System.out.print("one = " + num);
+                //     one++;
+                // }
+                // else if (num == 000000){
+                //     // System.out.print("Zero =" + num);
+                //     zero++;
+                // }
+                
+            }
+            System.out.println();
+            System.out.println(binaryArr[i]);
+            System.out.println(one);
+            System.out.println(zero);
+            one = 0;
+            zero = 0;
+     }
+
     }
     
     public static void main(String[] args)
